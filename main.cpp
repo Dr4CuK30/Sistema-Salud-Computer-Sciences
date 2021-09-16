f1b14741d6f30e280cd530a125fdd8e98ce71685
SantiagoHV <sherrerav@correo.udistrital.edu.co>
2021-09-16 15:40:02 -0500

    Inicio en UX

--- main.cpp
+++ main.cpp
@@ -6,16 +6,91 @@
 
 using namespace std;
 
+void empezarPrograma();
+void consultarPacientesPor();
+void usarTablaBasica(int filas, int columnas /*Cola etiquetas, datos*/);
+
 int main(int argc, char *argv[]) {
 	
-	ColaPrioridad cola(6);
-	cola.insertar(5);
-	cola.insertar(7);
-	cola.insertar(8);
-	cola.insertar(2);
-	cola.insertar(3);
-	//cola.insertar(1);
-	cola.imprimirArreglo();
+	empezarPrograma();
+	
     system("PAUSE");
     return EXIT_SUCCESS;
+}
+
+void empezarPrograma(){
+	//cargar archivos
+	
+	int opcion;
+	
+	while(opcion != 8){
+		cout<<"----------------------------------------------"<<endl;
+		cout<<"--------BIENVENIDO A SALDAVACUNA APP----------"<<endl;
+		cout<<"----------------------------------------------"<<endl;
+		
+		cout<<"1. Simular proceso de vacunacion completo"<<endl;
+		cout<<"2. Ingresar nuevo registro"<<endl;
+		cout<<"3. Consultar pacientes por..."<<endl;
+		cout<<"4. Ver todos los pacientes"<<endl;
+		cout<<"5. Ver todos las vacunas registradas"<<endl;
+		cout<<"6. Ver todas las EPSs"<<endl;
+		cout<<"7. Ver todas las IPSs"<<endl;
+		cout<<"8. SALIR"<<endl;
+		
+		switch(opcion){
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				consultarPacientesPor();
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			case 7:
+				break;
+			default:	
+				cout<<"Opcion no valida"<<endl<<endl;
+				break;
+		}
+	}	
+	
+	//guardar archivos
+}
+
+void consultarPacientesPor(){
+	int opcion;
+	
+	while(opcion != 5){
+		cout<<"---------------------------------"<<endl;
+		cout<<"Puede consultar los pacientes por: "<<endl;
+		cout<<"1. EPS"<<endl;
+		cout<<"2. Edad"<<endl;
+		cout<<"3. Vacunados con un tipo de vacuna"<<endl;
+		cout<<"4. Fecha de vacunacion"<<endl;
+		cout<<"5. Volver al menu principal"<<endl;
+		cout<<"---------------------------------"<<endl;
+		
+		switch(opcion){
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			default:	
+				cout<<"Opcion no valida"<<endl<<endl;
+				break;
+		}
+	}
+	
+
 }
