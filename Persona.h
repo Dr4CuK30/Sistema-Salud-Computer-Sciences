b6219131259a8b5d3f2cf591fb5a6bd1ee3efb71
David Saldarriaga <davidsaldarriaga.pardo@gmail.com>
2021-09-22 21:34:46 -0500

    Solución Bug Sapoperro

--- Persona.h
+++ Persona.h
@@ -27,8 +27,8 @@ class Persona{
 		long long int telefono_cel;
 		long long int telefono_fijo;
 		Fecha f_nacimiento;
-		Fecha f_primera_dosis;
-		Fecha f_segunda_dosis;
+		Fecha *f_primera_dosis;
+		Fecha *f_segunda_dosis;
 		Vacuna vacuna;
 	public:
 		Persona(){};
@@ -49,7 +49,7 @@ class Persona{
 		Identificacion getId(){
 			return id;
 		}
-		bool Vacunar(Vacuna vacuna);
+		bool Vacunar(Vacuna vacuna, Fecha *fecha);
 };
 
 //--------- CONSTRUCTORES -----------//
@@ -86,7 +86,7 @@ Persona::Persona(long long int numeroId,
 }
 
 //-------------- METODOS -------------------//
-bool Persona::Vacunar(Vacuna vacuna, Fecha fecha){
+bool Persona::Vacunar(Vacuna vacuna, Fecha *fecha){
 	if(this->f_segunda_dosis != NULL) return false;
 	this->vacuna = vacuna;
 	if(this->f_primera_dosis == NULL) this->f_primera_dosis = fecha;
