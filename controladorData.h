09a453b633b9cc49d3eaabaf7d7d50e85652e552
SantiagoHV <sherrerav@correo.udistrital.edu.co>
2021-09-22 19:18:14 -0500

    cambios pequeños para analisis de estructuras

--- controladorData.h
+++ controladorData.h
@@ -4,6 +4,8 @@
 #include "Lista.h"
 //#include "Persona.h"
 #include "Vacuna.h"
+
+#include "ArbolBinarioOrdenado.h"
 #include <iostream>
 
 
@@ -17,12 +19,22 @@ class ControladorData{
 		int cantidadVacunas;
 		int cantidadEPS;
 		int cantidadIPS;
-		int cantidadEPSVacuna;
-		int cantidadIPSVacuna;
+		int cantidadEPSVacunas;
+		int cantidadIPSVacunas;
 		int cantidadEPS_IPS;
 		
 		//---------------------listas generales-----------------------------------//
 		Lista <Vacuna> listaVacunas;
+		//Lista <Persona> listaPersonas;
+		//Lista <EPS> listaEPS;
+		//Lista <IPS> listaIPS;
+		//Lista <EPSVacuna>;
+		//Lista <IPSVacuna>;
+		//Lista <EPS_IPS>;
+		
+		//---------------------Arboles ordenados----------------------------------//
+		
+		
 		
 		
 	
